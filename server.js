47d2542e5724e189fb7ecc6ba45a4baaec882b62
luisejv <luis.jauregui@utec.edu.pe>
2021-02-06 18:05:44 -0500

    retrying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/client"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/client/index.html"));
