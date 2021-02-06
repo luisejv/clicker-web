7b7710a1fad8b14eea46d50785c13b41308cbcba
luisejv <luis.jauregui@utec.edu.pe>
2021-02-06 17:50:07 -0500

    retrying

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
-/* app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/client/index.html'));
-}); */
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/client/index.html"));
+});
 
 app.listen(process.env.PORT || 8080);
