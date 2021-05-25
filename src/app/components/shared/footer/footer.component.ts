b95035ce2bbc2e8c62aa6e33d506420032f8c76c
luisejv <luis.jauregui@utec.edu.pe>
2021-05-24 19:53:19 -0500

    cambios Sebastian 23-05

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarSearchFilter } from 'src/app/core/interfaces/car-search-filter';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  ruta: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private storageService: StorageService, private router: Router) {
+    if (this.storageService.isLoggedIn()) {
+      this.ruta = '/dashboard/registrar-carro';
+    } else {
+      this.ruta = '/auth';
+    }
   }
 
+  goToCarsSubset(subset: string): void {
+    const body: CarSearchFilter = {
+      carSubset: subset,
+    };
+    this.router
+      .navigateByUrl('/home', { skipLocationChange: true })
+      .then(() => {
+        this.router.navigate(['/inventory-listings'], { queryParams: body });
+      });
+  }
+
+  ngOnInit(): void {}
 }
